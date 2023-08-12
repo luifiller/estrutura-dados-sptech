d7cadb2d5c849121e98e2d8d587889056155ee95
luizfiller <luiz.fsilva@sptech.school>
2023-08-12 11:20:58 -0300

    Add - exercício de interface

---
+++ Exercícios/08-08/exemplo-interface/src/main/java/org/example/Perfume.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Perfume extends Produto {
+    private String fragrancia;
+
+    public Perfume(Integer codigo, String descricao, Double preco, String fragrancia) {
+        super(codigo, descricao, preco);
+        this.fragrancia = fragrancia;
+    }
+
+    @Override
+    public Double getValorTributo() {
+        return getPreco() * 0.27;
+    }
+
+    @Override
+    public String toString() {
+        return "Perfume{" +
+                "fragrancia='" + fragrancia + '\'' +
+                "} " + super.toString();
+    }
+}
